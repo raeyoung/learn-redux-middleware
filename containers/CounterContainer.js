b16bd3db01f0ae4211843447ab0f3dc19581c60f
unknown <raeyoung233@gmail.com>
2021-03-22 22:24:33 +0900

    Setting

---
+++ containers/CounterContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+    return (
+        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    )
+}
+
+export default connect(
+    state => ({
+        number: state.number
+    }),
+    {
+        increase,
+        decrease
+    }
+)(CounterContainer);
